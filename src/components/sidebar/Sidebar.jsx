53caae5013161b6affc4790020c251ea0ed3328e
Jessica Arf <jessicalealarf8@gmail.com>
2022-10-30 16:31:05 -0300

    component Sidebar created

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,57 @@
 import "./sidebar.css"
+import Logo from "../../assets/logo.svg"
 
 export const Sidebar = () => {
-  return (
-    <div>sidebar</div>
-  )
-};
+    return (
+        <aside className="aside">
+            <a href="#home" className="nav__logo">
+                <img src={Logo}alt="logo"/>
+            </a>
+            <nav className="nav">
+                <div className="nav__menu">
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link">
+                              <i className="icon-home"></i>
+                              </a>
+                        </li>
+                    </ul>
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <a href="#about" className="nav__link">
+                              <i className="icon-book-open"></i>
+                              </a>
+                        </li>
+                    </ul>
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <a href="#resume" className="nav__link">
+                              <i className="icon-graduation"></i>
+                              </a>
+                        </li>
+                    </ul>
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <a href="#portfolio" className="nav__link">
+                              <i className="icon-layers"></i>
+                              </a>
+                        </li>
+                        </ul>
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <a href="#contact" className="nav__link">
+                              <i className="icon-envelope"></i>
+                              </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div className="nav__footer">
+              <span className="copyright"> &copy; 2022 - 2023</span>
+            </div>
 
+
+        </aside>
+
+    )
+};
